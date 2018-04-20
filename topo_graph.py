e6c7f20bfdc0d5dcca4a84db92eb75114ff2fe99
Sirius1996 <976952479@qq.com>
2018-04-20 16:03:08 +0800

    gouzaotuopu

---
+++ topo_graph.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import networkx as nx
+from networkx.readwrite import json_graph
+import json
+
+#卫星网络，两个
+SatG1=nx.random_regular_graph(2,2)
+SatG2=nx.random_regular_graph(2,2)

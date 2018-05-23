69a187d0a7491642c9850376f823a3f0d23aaa66
Sirius1996 <976952479@qq.com>
2018-05-24 01:20:38 +0800

    updata

--- dict.py
+++ dict.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import json
 wordArr=[]
 expArr=[]
 with open("dict.txt") as inFile:
@@ -20,13 +21,40 @@ with open("dict.txt") as inFile:
                     pass
                 else:
                     exp=exp+tempI
-
-
             wordArr.append(word)
             expArr.append(exp.decode('utf-8'))
             word=""
             exp=""
 
-print wordArr
-for item in expArr:
-    print item.encode('utf-8')
+# print wordArr
+# for item in expArr:
+#     print item.encode('utf-8')
+
+retArr=[]
+for item in range(0,506):
+    tempArr=[]
+    tempExp=""
+    for j in range(0,len(expArr[item])):
+        tempExp=tempExp+expArr[item][j].encode('utf-8')
+    tempArr.append("firefly forest")
+    tempArr.append(wordArr[item])
+    tempArr.append(tempExp)
+    tempArr.append(0)
+    tempArr.append(0)
+    tempArr.append(0.0002)
+    retArr.append(tempArr)
+
+f=open('import.txt','w')
+f.write('"user_name","word","interpret","likes","hates","score"\n')
+for item in range(0,506):
+    tempExp=""
+    for j in range(0,len(expArr[item])):
+        tempExp=tempExp+expArr[item][j].encode('utf-8')
+    f.write('"firefly forest","')
+    f.write(wordArr[item])
+    f.write('","')
+    f.write(tempExp)
+    f.write('",')
+    f.write('0,')
+    f.write('0,')
+    f.write('0.0002\n')
